6f880877ad1ebc72b5300a2815ee4dc54a9da236
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-05-19 20:21:21 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    email: str = ''
+    password: str = ''
+    first_name: str = ''
+    last_name: str = ''
