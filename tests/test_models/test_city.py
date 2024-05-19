b31bf1636adbc977090cb91997dac032242bc78c
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-05-19 11:36:53 +0300

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -8,24 +8,45 @@ from tests.test_models.test_base_model import test_basemodel
 from models.city import City
 
 
-class test_City(test_basemodel):
+class TestCityModel(unittest.TestCase):
     """city test """
+    def setUp(self):
+        self.city_model = City()
 
-    def __init__(self, *args, **kwargs):
-        """init """
-        super().__init__(*args, **kwargs)
-        self.name = "City"
-        self.value = City
+    def tearDown(self):
+        """ """
+        del self.city_model
 
-    def test_state_id(self):
-        """state id unittest """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
+    def test_instance_creation(self):
+        """ test instance"""
+        self.assertIsInstance(self.city_model, City)
+        self.assertTrue(hasattr(self.city_model, 'id'))
+        self.assertTrue(hasattr(self.city_model, 'created_at'))
+        self.assertTrue(hasattr(self.city_model, 'updated_at'))
+        self.assertTrue(hasattr(self.city_model, 'state_id'))
+        self.assertTrue(hasattr(self.city_model, 'name'))
 
-    def test_name(self):
-        """name unittest """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def test_string_representation(self):
+        """string test"""
+        string_repr = str(self.city_model)
+        self.assertIn("[City]", string_repr)
+        self.assertIn("id", string_repr)
+        self.assertIn("created_at", string_repr)
+        self.assertIn("updated_at", string_repr)
+        self.assertIn("state_id", string_repr)
+        self.assertIn("name", string_repr)
+
+    def test_to_dict_method(self):
+        """test to dictionary"""
+        city_dict = self.city_model.to_dict()
+        self.assertIsInstance(city_dict, dict)
+        self.assertEqual(city_dict['__class__'], 'City')
+        self.assertIn('id', city_dict)
+        self.assertIn('created_at', city_dict)
+        self.assertIn('updated_at', city_dict)
+        self.assertIn('state_id', city_dict)
+        self.assertIn('name', city_dict)
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.m 
